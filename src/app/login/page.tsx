1663650d226b50b892984ca5bced6be295e1e785
PremveerSingh195 <prem.veer2836@gmail.com>
2025-07-22 09:35:30 +0530

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
 import React from "react";
 
 function page() {
-  return <div className="h-screen bg-amber-600">page</div>;
+  const handleLoginSubmit = () => {};
+  return (
+    <div className="h-screen bg-amber-600 flex justify-center items-center">
+      <div className="w-96 h-96 bg-gray-600 rounded-2xl">
+        <form
+          onSubmit={handleLoginSubmit}
+          className="flex justify-center items-center flex-col"
+        >
+          <h1 className="text-3xl">Welcome Back</h1>
+          <div className="flex flex-col justify-center items-start">
+            <label>Username</label>
+            <input type="text" name="" id="" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default page;
