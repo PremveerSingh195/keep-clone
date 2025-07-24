dba1c161380267c9ab8e055cef630c1aa10d6ef1
PremveerSingh195 <prem.veer2836@gmail.com>
2025-07-24 22:02:25 +0530

    dsf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <div className="bg-red-500 h-screen flex justify-center items-center">
       <Sighup />
-      {/* cdjsfkjdhfkjhadskjfhdskjh */}
     </div>
   );
 }
